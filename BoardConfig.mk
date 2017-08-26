76bea568690634474a40f4e1f247cbac954ac273
Vineeth Penugonda <vineeth.pen@outlook.com>
2017-08-26 23:25:44 +0530

    initial commit
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2015-2016 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/lge/mh
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8937_32
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+
+TARGET_ARCH := arm64
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+# Partitions (TBD)
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE :=      # x * 1024 mmcblk0p1X
+BOARD_RECOVERYIMAGE_PARTITION_SIZE :=   # x * 1024 mmcblk0p1X
+BOARD_SYSTEMIMAGE_PARTITION_SIZE :=   # x * 1024 mmcblk0p3X
+BOARD_USERDATAIMAGE_PARTITION_SIZE :=  # x * 1024 mmcblk0p3X
+
+# KERNEL
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.hardware=mh buildvariant=user
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_PREBUILT_KERNEL := device/lge/mh/recovery/kernel
+
+# TWRP (TBD)
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+# RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INCLUDE_CRYPTO := true
+# BOARD_SUPPRESS_EMMC_WIPE := true
+# TW_SCREEN_BLANK_ON_BOOT := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
